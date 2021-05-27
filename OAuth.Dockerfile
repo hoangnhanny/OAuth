156bdcbf3770213525b6b4ac45ff90e7dcdfe47d
HoangNhan <hoangnhan280197@gmail.com>
2021-05-27 09:23:11 +0700

    init

---
+++ OAuth.Dockerfile
@@ -0,0 +1,5 @@
+FROM openjdk:11-jdk-alpine
+
+COPY ./OAuth /source/OAuth
+
+
---
+++ SV/src/main/java/SV/SV/SvApplication.java
@@ -0,0 +1,13 @@
+package SV.SV;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SvApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SvApplication.class, args);
+	}
+
+}
